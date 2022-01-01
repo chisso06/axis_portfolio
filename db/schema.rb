bc1e581fec75e49b9bb6f91c5bbcb3fdcf44019e
鈴木美優 <miyu@suzukimiyuus-MacBook-Air.local>
2022-01-02 01:05:10 +0900

    create users-table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_31_165832) do
+ActiveRecord::Schema.define(version: 2022_01_01_142550) do
 
   create_table "posts", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
@@ -22,6 +22,17 @@ ActiveRecord::Schema.define(version: 2021_12_31_165832) do
     t.text "background"
     t.text "do"
     t.text "notice"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.string "email"
+    t.string "grade"
+    t.string "image"
+    t.string "password"
+    t.string "real_name"
   end
 
 end
