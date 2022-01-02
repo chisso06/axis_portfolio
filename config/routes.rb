56d673b63331e41975ca826777b8c61bb299f8e7
YHTR0257 <grandflight@outlook.com>
2022-01-02 13:43:51 +0900

    become only setting

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,6 @@
 Rails.application.routes.draw do
-  get '/posts/new' => 'posts#new'
-  post '/posts/create' => 'posts#create'
-  get '/posts/:id/show' => 'posts#show'
-  get '/posts/:id/edit' => 'posts#edit'
-  post '/posts/:id/update' => 'posts#update'
-  post '/posts/:id/destroy' => 'posts#destroy'
+  get 'users/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get '/' => 'home#top'
-
-  get '/table/post' => 'table#post'
+  get 'users/index' => 'users#index'
+  get "/" => "home#top"
 end
