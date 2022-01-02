e91cfb63006aab20940ef2a243953449e8f12664
YHTR0257 <grandflight@outlook.com>
2022-01-02 11:48:09 +0900

    become only index related

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,5 @@
-class UsersController < ApplicationController
+class UsersController < ApplicationController  
   def index
     @users = User.all
-  end
-
-  def edit
-    @user = User.find_by(id: params[:id])
   end
 end
