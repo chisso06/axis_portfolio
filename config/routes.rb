8712d3aa3a1faca225787e6682cf43c75e08cc24
YHTR0257 <grandflight@outlook.com>
2022-01-05 10:21:38 +0900

    20220105

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  get 'users/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get 'users/index' => 'users#index'
+  get 'users/:user_id/setting' => 'users#setting'
   get "/" => "home#top"
 end
