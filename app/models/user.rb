0b69b02bfcda0f270ea44dd3ad365fbce82e7adb
鈴木美優 <miyu@suzukimiyuus-MacBook-Air.local>
2022-02-20 17:18:28 +0900

    password security

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,19 @@
 class User < ApplicationRecord
-  with_options presence: true do
-    validates :name
-    validates :real_name
-    validates :grade
-    validates :email
-    validates :password
-  end
+
+  has_secure_password
+
+  validates :name, presence: true
+  validates :real_name, presence: true
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true,
+                    length: { minimum: 6 },
+                    format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: { case_sensitive: false }
 
   validates :grade, numericality: { in: 0..10 }, format: { with: /\A[0-9]+\z/ }
+
+  validates :password, presence: true
 
   def posts
     return Post.where(user_id: self.id)
---
+++ db/migrate/20220220061025_add_culumn_to_users.rb
@@ -0,0 +1,6 @@
+class AddCulumnToUsers < ActiveRecord::Migration[6.1]
+  def change
+    add_column :users, :password_digest, :string
+    remove_column :users, :password, :string
+  end
+end
