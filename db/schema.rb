6da3e6b6a754bb79bd5f84332be3102fb92ed224
YHTR0257 <grandflight@outlook.com>
2022-07-15 13:10:11 +0900

    Add image_name column to Users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_20_061025) do
+ActiveRecord::Schema.define(version: 2022_07_15_040819) do
 
   create_table "posts", force: :cascade do |t|
     t.integer "user_id"
@@ -35,6 +35,8 @@ ActiveRecord::Schema.define(version: 2022_02_20_061025) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "password_digest"
+    t.string "avatar"
+    t.string "image_name"
   end
 
 end
